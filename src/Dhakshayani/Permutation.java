210ab59e83c7dc188233ccbe34ddb3af4eb6aca1
Dhakshayani Nalla <dhakshayani.nalla@serole.com>
2022-06-29 14:39:30 +0530

    moving files to src by dhakshayani

---
+++ src/Dhakshayani/Permutation.java
@@ -0,0 +1,41 @@
+package Dhakshayani;
+
+public class Permutation {
+    public static void main(String[] args)
+    {
+        int[]  a= { 1, 2, 3};
+        printPermutation(a,0);
+    }
+    static void printPermutation(int[] a,int index)
+    {
+        if(index==a.length)
+        {
+            printArray(a);
+            return;
+        }
+        for(int i=index;i<a.length;i++)
+        {
+            swapping(a,i,index);
+            printPermutation(a,index+1);
+            swapping(a,i ,index);
+        }
+
+
+    }
+    static void printArray(int[] a)
+    {
+        for(int i=0;i<a.length;i++)
+        {
+            System.out.print(a[i]+ "");
+        }
+        System.out.println("");
+    }
+    static void swapping(int[] a,int i,int j)
+    {
+        int temp = a[i];
+        a[i]= a[j];
+        a[j]= temp;
+
+    }
+
+}
