726aefe62d5ec3724471ed68902807bd62d8d199
srujana <kadalisujana@gmai.com>
2022-08-11 21:38:36 +0530

    Aug11 Assignment

---
+++ src/src/Srujana/loops/aug5/AssignmentAug11.java
@@ -0,0 +1,15 @@
+package src.Srujana.loops.aug5;
+
+public class AssignmentAug11 {
+    public static void main(String[] args) {
+        int i, rows = 3;
+        for (i = 7 * rows+1; i > 1; i--) {
+            System.out.print("*");
+        }
+        System.out.println("\n" +"       srujana        "+"\n");
+        for (i = 7 * rows+1; i > 1; i--) {
+            System.out.print("*");
+
+        }
+    }
+}
